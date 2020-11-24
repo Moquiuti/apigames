37486c3afc78e48e98d44001d64a17830ba95599
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-24 15:45:20 -0400

    Recurso de deletar e de inserir novo game

--- index.js
+++ index.js
@@ -49,6 +49,33 @@ app.get('/game/:id', (req, res) => {
   }
 });
 
+app.post('/game', (req, res) => {
+    var{title, price, year} = req.body;
+    DB.games.push({
+      id: 124,
+      title,
+      price,
+      year
+    });
+    res.sendStatus(200);
+});
+
+app.delete('/game/:id', (req, res) => {
+  if(isNaN(req.params.id)){
+      res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var index = DB.games.findIndex(g => g.id == id);
+
+    if(index == -1){
+      res.sendStatus(404);
+    } else {
+      DB.games.splice(index,1);
+      res.sendStatus(200);
+    }
+  }
+});
+
 app.listen(8081, () => {
   console.log("api rodando!");
 });
