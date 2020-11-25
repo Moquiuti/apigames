09655e01f715fda11f6e3d0f6aa734a9b29032d9
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-25 16:41:39 -0400

    Adicionado cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express =  require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 var DB = {
   games : [
